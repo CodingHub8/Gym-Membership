69f2a6492f89ba29afa4504783956a0eb6eaad20
ManBil <manbil6146@gmail.com>
2025-01-28 19:27:59 +0800

    Added navigation between user and admin

--- app/src/main/java/com/example/gym_membership/Admin/Login.java
+++ app/src/main/java/com/example/gym_membership/Admin/Login.java
@@ -1,17 +1,25 @@
 package com.example.gym_membership.Admin;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.gym_membership.R;
+import com.example.gym_membership.databinding.AdminActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
+    AdminActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.admin_activity_login);
+        binding = AdminActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    public void toUserPage(View view){
+        Intent intent = new Intent(this, com.example.gym_membership.User.Login.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Reuse the existing instance if it's already at the top
+        startActivity(intent);
+        finish();
     }
 }
